17f14cfcb1e25aa2836e77cc2a13debe7722c19b
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-02-25 14:25:01 +0500

    hw2 complete

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,33 +1,16 @@
 package edu.java;
 
 import edu.java.configuration.ApplicationConfig;
-import edu.java.github.GitHubClient;
-import edu.java.stackoverflow.StackOverflowClient;
-import edu.java.stackoverflow.StackOverflowResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(ApplicationConfig.class)
-public class ScrapperApplication implements CommandLineRunner {
+public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
-    }
-
-    @Autowired
-    private GitHubClient gitHubClient;
-    @Autowired
-    private StackOverflowClient stackOverflowClient;
-
-    @Override
-    public void run(String... args) throws Exception {
-//        GitHubResponse response1 = gitHubClient.fetchUpdate("lenurseytasanov", "java-course-2024");
-//        System.out.println(response1.updatedAt());
-
-//        StackOverflowResponse response = stackOverflowClient.fetchUpdate(78050639);
-//        System.out.println(response.getUpdateAt());
     }
 }
