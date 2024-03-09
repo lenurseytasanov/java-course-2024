03f0538e14cbf0a0c1ba7e775f17b96ef5cc9d85
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-03-09 13:14:44 +0500

    hw4 complete

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,5 +1,17 @@
 package edu.java.scrapper;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
@@ -11,7 +23,7 @@ public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
     static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:15")
+        POSTGRES = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("scrapper")
             .withUsername("postgres")
             .withPassword("postgres");
@@ -21,7 +33,22 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        try (Connection connection = DriverManager.getConnection(
+            c.getJdbcUrl(), c.getUsername(), c.getPassword()
+        )) {
+            Path changelog = new File(".").toPath().toAbsolutePath()
+                .getParent().getParent().resolve("migrations");
+            ResourceAccessor changelogDir = new DirectoryResourceAccessor(changelog);
+
+            Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new Liquibase("master.xml", changelogDir, database);
+
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
---
+++ scrapper/src/test/java/edu/java/scrapper/SimpleTest.java
@@ -0,0 +1,12 @@
+package edu.java.scrapper;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest extends IntegrationTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
